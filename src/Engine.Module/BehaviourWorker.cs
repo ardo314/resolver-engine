d1a603d76a92b08df02bc8f018ceb8fe9d2d726e
Leonard Hansing <lhansing92@gmail.com>
2026-03-14 18:22:57 +0000

    changes

--- src/Engine.Module/BehaviourWorker.cs
+++ src/Engine.Module/BehaviourWorker.cs
@@ -5,7 +5,7 @@ namespace Engine.Module;
 public abstract class BehaviourWorker<T>
     where T : IBehaviour
 {
-    public EntityId EntityId { get; set; }
+    public EntityId EntityId { get; private set; }
 
     public virtual Task OnAddedAsync(CancellationToken ct = default) => Task.CompletedTask;
 
