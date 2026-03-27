2ad9da0ebd51735bb18b7383233aeadc9d66341c
ardo314 <lhansing92@gmail.com>
2026-03-27 17:18:52 +0000

    start handler

--- engine/backend/src/index.ts
+++ engine/backend/src/index.ts
@@ -1,2 +1,9 @@
+import { connect } from "nats";
+import { EntityHandler } from "./entity-handler.js";
 export { EntityRepository } from "./entity-repository.js";
-export { EntityHandler } from "./entity-handler.js";
+
+const nc = await connect();
+const handler = new EntityHandler(nc);
+await handler.listen();
+
+console.log("Backend listening on NATS");
