6d88e02f38089e9df405ce500317f8822da755d8
Leonard Hansing <leonard.hansing@wandelbots.com>
2026-04-10 15:02:39 +0200

    fix editor

--- engine/editor/entrypoint.sh
+++ engine/editor/entrypoint.sh
@@ -16,6 +16,14 @@ case "$BASE_PATH" in
   *)  BASE_PATH="$BASE_PATH/" ;;
 esac
 
+if [ "$BASE_PATH" = "/" ]; then
+  BASE_PATH_NO_SLASH="/__no_base_path_redirect__"
+  ROOT_LOCATION_DIRECTIVE="try_files /index.html =404;"
+else
+  BASE_PATH_NO_SLASH="${BASE_PATH%/}"
+  ROOT_LOCATION_DIRECTIVE="return 302 ${BASE_PATH};"
+fi
+
 # --- Generate runtime configuration ---
 cat > "$HTML_DIR/env.js" <<EOF
 window.__ENV__ = {
@@ -26,6 +34,6 @@ EOF
 
 # --- Generate nginx config from template ---
 NGINX_CONF="/etc/nginx/conf.d/default.conf"
-sed "s|__BASE_PATH__|${BASE_PATH}|g" /etc/nginx/conf.d/default.conf.template > "$NGINX_CONF"
+sed -e "s|__ROOT_LOCATION_DIRECTIVE__|${ROOT_LOCATION_DIRECTIVE}|g" -e "s|__BASE_PATH_NO_SLASH__|${BASE_PATH_NO_SLASH}|g" -e "s|__BASE_PATH__|${BASE_PATH}|g" /etc/nginx/conf.d/default.conf.template > "$NGINX_CONF"
 
 exec nginx -g 'daemon off;'
